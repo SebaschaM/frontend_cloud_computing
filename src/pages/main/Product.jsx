9c829cfbc5b335a3057a168a6d240679bddbb280
Sebastián <schaquila@autonoma.edu.pe>
2023-06-26 01:47:14 -0500

    SelectProductsByStock in LocalStorage / Bugs fixed

--- src/pages/main/Product.jsx
+++ src/pages/main/Product.jsx
@@ -1,6 +1,5 @@
 import Header from "../../components/Header_Logeado";
 import wine from "../../assets/ImgWine.png";
-import product1 from "../../assets/Wine.png";
 import { useEffect, useState, useContext } from "react";
 import useFetch from "../../hooks/useFetch.JSX";
 import styles from "../../styles/Product.module.css";
@@ -13,6 +12,7 @@ function Product() {
   const [count, setCount] = useState(1);
   const { getCategoryList, getProductByBranch, getDetailProduct } = useFetch();
   const [productData, setProductData] = useState(null);
+  const [modalCount, setModalCount] = useState(1);
   const { state } = useContext(StoreContext);
 
   const handleCategoryList = async () => {
@@ -38,10 +38,57 @@ function Product() {
     try {
       const data = await getProductByBranch(state.selectedBranchId);
       setProductByBranch(data);
+      // Eliminar el arreglo de productos del localStorage al cambiar de sucursal
+      localStorage.removeItem("cart");
       console.log(data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const addToCart = (selectedProduct) => {
+    // Obtener el carrito actual del localStorage
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const existingProduct = cartItems.find(
+      (item) => item.id === selectedProduct.id
+    );
+
+    if (existingProduct) {
+      // Si el producto existe, incrementar la cantidad
+      if (existingProduct.quantity + modalCount <= selectedProduct.stock) {
+        existingProduct.quantity += modalCount;
+      } else {
+        // Mostrar mensaje de error o realizar alguna acción adecuada cuando se excede el stock
+        console.log("No se puede agregar más del stock máximo");
+        return; // Salir de la función sin actualizar el carrito
+      }
+    } else {
+      const productToAdd = {
+        id: selectedProduct.id,
+        name: selectedProduct.name,
+        price: selectedProduct.price,
+        quantity:
+          modalCount <= selectedProduct.stock
+            ? modalCount
+            : selectedProduct.stock,
+      };
+
+      cartItems.push(productToAdd);
+    }
+
+    // Guardar el carrito actualizado en el localStorage
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+
+    // Cerrar el modal y reiniciar el contador
+    setShowModalProduct(false);
+    setModalCount(1);
+  };
+
+  const getCartItemCount = (productId) => {
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    const existingProduct = cartItems.find((item) => item.id === productId);
+    return existingProduct ? existingProduct.quantity : 0;
   };
 
   useEffect(() => {
@@ -121,9 +168,20 @@ function Product() {
                                   S/ {product.price}
                                 </p>
                               </div>
-                              <div
+                              <button
                                 className={styles.btn_add}
-                                onClick={(e) => e.stopPropagation()}
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  const newCount = count;
+                                  if (newCount <= product.stock) {
+                                    addToCart(product);
+                                    setCount(newCount);
+                                  } else {
+                                    console.log(
+                                      "No se puede agregar más del stock máximo"
+                                    );
+                                  }
+                                }}
                               >
                                 <svg
                                   className={styles.svg_add}
@@ -137,7 +195,7 @@ function Product() {
                                     className="fill-000000"
                                   ></path>
                                 </svg>
-                              </div>
+                              </button>
                             </div>
                           </div>
                         </div>
@@ -161,13 +219,11 @@ function Product() {
       </div>
 
       {/* Modal */}
-      {/* Modal */}
       {showModalProduct && productData && (
         <div
           className={styles.modal}
           onClick={() => {
             setShowModalProduct(false);
-            setCount(1);
           }}
         >
           <div
@@ -220,8 +276,8 @@ function Product() {
                   <button
                     className={styles.btn_event}
                     onClick={() => {
-                      if (count > 1) {
-                        setCount((prevCount) => prevCount - 1);
+                      if (modalCount > 1) {
+                        setModalCount((prevCount) => prevCount - 1);
                       }
                     }}
                   >
@@ -240,10 +296,20 @@ function Product() {
                       ></path>
                     </svg>
                   </button>
-                  <p className={styles.product_quantity}>{count}</p>
+                  <p className={styles.product_quantity}>{modalCount}</p>
                   <button
                     className={styles.btn_event}
-                    onClick={() => setCount((count) => count + 1)}
+                    onClick={() => {
+                      if (
+                        modalCount <
+                        Math.min(
+                          productData.stock,
+                          productData.stock - getCartItemCount(productData.id)
+                        )
+                      ) {
+                        setModalCount((prevCount) => prevCount + 1);
+                      }
+                    }}
                   >
                     <svg
                       strokeWidth="1.9"
@@ -265,9 +331,12 @@ function Product() {
             </div>
             <div className={styles.modal_cart_option}>
               <p className={styles.product_price_cart}>
-                S/ {count * productData.price}.00
+                S/ {modalCount * productData.price}.00
               </p>
-              <button className={styles.btn_event_add}>
+              <button
+                className={styles.btn_event_add}
+                onClick={() => addToCart(productData)}
+              >
                 <div className={styles.btn_cart}>
                   <p className={styles.txt_add_cart}>Agregar al carrito</p>
                   <svg
